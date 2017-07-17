c9b41da0a8043af5636a9212b826d8dd621b2964
Alexander Bragin <alexander.bragin@gmail.com>
2017-07-18 00:53:00 +0300

    Added Kotlin support.

---
+++ modules/examples/src/androidTest/java/com/roxiemobile/examples/materialdesign/ApplicationTest.kt
@@ -0,0 +1,6 @@
+package com.roxiemobile.examples.materialdesign
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
